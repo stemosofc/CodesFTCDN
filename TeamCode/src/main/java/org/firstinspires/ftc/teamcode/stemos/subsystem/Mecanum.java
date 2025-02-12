820b55169cd38164cbaa8f55d2a44947f1b56cd2
Enzo-Coutinho <enzo@stemos.com.br>
2025-02-12 16:36:43 -0300

    remove functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stemos/subsystem/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stemos/subsystem/Mecanum.java
@@ -14,17 +14,11 @@ import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.stemos.Constants;
 
 public class Mecanum {
-    public static final double MAX_VELOCITY = 12; // in/s
-    private static final double CPR = 1680;
-    private static final double WHEEL_DIAMETER = 4;
-
     private final DcMotorEx motorEsquerdaFrente;
     private final DcMotorEx motorDireitaFrente;
     private final DcMotorEx motorEsquerdaTras;
     private final DcMotorEx motorDireitaTras;
     private final IMU imu;
-
-    private double factorConversionEncoder = 1;
 
     public Mecanum(OpMode opMode) {
         motorEsquerdaFrente = opMode.hardwareMap.get(DcMotorEx.class, Constants.DriveTrainMotorsNames.MOTOR_ESQUERDA_FRENTE);
@@ -79,54 +73,6 @@ public class Mecanum {
         motorDireitaFrente.setPower(frontRightPower);
         motorEsquerdaTras.setPower(backLeftPower);
         motorDireitaTras.setPower(backRightPower);
-    }
-
-    public double getLinearDistance()
-    {
-        return ((motorEsquerdaFrente.getCurrentPosition() + motorDireitaFrente.getCurrentPosition()) * factorConversionEncoder) / 2;
-    }
-
-    public double getLinearDistanceOfOneMotor()
-    {
-        return motorEsquerdaFrente.getCurrentPosition() * factorConversionEncoder;
-    }
-
-    public void setLinearTarget(double target)
-    {
-        DcMotorEx[] motors = {motorEsquerdaFrente, motorDireitaFrente, motorDireitaTras, motorEsquerdaTras};
-        for(DcMotorEx motor : motors)
-        {
-            motor.setTargetPosition(motor.getCurrentPosition() + (int)(target / factorConversionEncoder));
-        }
-    }
-
-    public boolean atTarget()
-    {
-        return motorEsquerdaFrente.isBusy();
-    }
-
-    public void setPIDFForPosition(double p, double i, double d)
-    {
-        PIDFCoefficients pidfNew = new PIDFCoefficients(p, i, d, 0);
-        DcMotorEx[] motors = {motorEsquerdaFrente, motorDireitaFrente, motorDireitaTras, motorEsquerdaTras};
-        DcMotorControllerEx[] motorControllerEx = new DcMotorControllerEx[4];
-        for(int count = 0; count < motors.length; count++)
-        {
-            motorControllerEx[count] = (DcMotorControllerEx) motors[count].getController();
-            motorControllerEx[count].setPIDFCoefficients(motors[count].getPortNumber(), DcMotor.RunMode.RUN_TO_POSITION, pidfNew);
-        }
-    }
-
-    public void setPIDFForVelocity(double p, double i, double d, double f)
-    {
-        PIDFCoefficients pidfNew = new PIDFCoefficients(p, i, d, f);
-        DcMotorEx[] motors = {motorEsquerdaFrente, motorDireitaFrente, motorDireitaTras, motorEsquerdaTras};
-        DcMotorControllerEx[] motorControllerEx = new DcMotorControllerEx[4];
-        for(int count = 0; count < motors.length; count++)
-        {
-            motorControllerEx[count] = (DcMotorControllerEx) motors[count].getController();
-            motorControllerEx[count].setPIDFCoefficients(motors[count].getPortNumber(), DcMotor.RunMode.RUN_USING_ENCODER, pidfNew);
-        }
     }
 
     public double getHeading()
